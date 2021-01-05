c34473b1f0ea3dfcf5d9e37c58bd14b7f9fb93fa
nicolepompei <nicole.lynn.pompei@gmail.com>
2021-01-05 11:01:12 -0500

    node class completed

---
+++ src/main/java/Node.java
@@ -0,0 +1,38 @@
+public class Node <SomeType>{
+    //reference to the next node in the chain, or null if there isn't one
+    private Node next;
+    //data carried by this node. could be of any type if needed
+    private SomeType obj;
+    private int index = 0;
+
+    //Node constructor
+    public Node(SomeType obj, Node next){
+        this.obj = obj;
+        this.next = next;
+    }
+
+    public Node getNext(){
+        return next;
+    }
+
+    public void setNext(){
+        this.next = next;
+    }
+
+    public SomeType getObj(){
+        return obj;
+    }
+
+    public void setObj(SomeType obj){
+        this.obj = obj;
+    }
+
+    public int getIndex(){
+        return index;
+    }
+
+    public void setIndex(int index){
+        this.index = index;
+    }
+
+}
